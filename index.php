0a79ee6f9d487034504769a9de7f354c0b6536f2
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-05-25 14:36:03 +0200

    fixed print with description

--- index.php
+++ index.php
@@ -13,16 +13,44 @@ $avatar=new Movie('Avatar','Fantascienza','Inglese');
 $batman=new Movie('Batman','Polizzesco','Americano');
 $romanzo_criminale=new Movie('Romanzo Criminale','Drammatico','Italiano');
 
+echo "<h2>Print all attributes</h2>";
+echo "<h4> Avatar </h4>";
+echo $avatar->title . "<br>";
+echo $avatar->genre . "<br>";
+echo $avatar->language . "<br>";
+echo "<h4> Batman </h4>";
+echo $batman->title . "<br>";
+echo $batman->genre . "<br>";
+echo $batman->language . "<br>";
+echo "<h4> Romanzo Criminale </h4>";
+echo $romanzo_criminale->title . "<br>";
+echo $romanzo_criminale->genre . "<br>";
+echo $romanzo_criminale->language . "<br>";
+
+echo "<h2>Print all attributes with Method</h2>";
+echo "<h4> Avatar </h4>";
 echo $avatar->getTitle() . "<br>";
+echo $avatar->getGenre() . "<br>";
+echo $avatar->getLanguage() . "<br>";
+echo "<h4> Batman </h4>";
 echo $batman->getTitle() . "<br>";
+echo $batman->getGenre() . "<br>";
+echo $batman->getLanguage() . "<br>";
+echo "<h4> Romanzo Criminale </h4>";
 echo $romanzo_criminale->getTitle() . "<br>";
+echo $romanzo_criminale->getGenre() . "<br>";
+echo $romanzo_criminale->getLanguage() . "<br>";
 
 $avatar->setTitle('Avatar 2.0');
 $batman->setTitle('Batman: Arkham Asylium');
 $romanzo_criminale->setTitle('Romanzo Criminale: La Serie');
 
+echo "<h2>Print Title with Method after Set Title Method</h2>";
+echo "<h4> Avatar </h4>";
 echo $avatar->getTitle() . "<br>";
+echo "<h4> Batman </h4>";
 echo $batman->getTitle() . "<br>";
+echo "<h4> Romanzo Criminale </h4>";
 echo $romanzo_criminale->getTitle() . "<br>";
 
-var_dump($avatar,$batman,$romanzo_criminale);
+//var_dump($avatar,$batman,$romanzo_criminale);
